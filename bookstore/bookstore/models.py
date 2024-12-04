a1a11bf3de2f78bbae15e5099d9439060c4f2322
QuocBao <baovo32911@gmail.com>
2024-12-04 23:15:32 +0700

    Update model

--- bookstore/bookstore/models.py
+++ bookstore/bookstore/models.py
@@ -6,6 +6,7 @@ from sqlalchemy import (
     Enum,
     DateTime,
 )
+from sqlalchemy.dialects.mssql.information_schema import mssql_temp_table_columns
 from sqlalchemy.orm import relationship
 from bookstore import app, db
 from enum import Enum as PyEnum
@@ -24,8 +25,8 @@ class PhuongThucThanhToan(PyEnum):
 
 class TheLoai(db.Model):
     ma_the_loai = Column(Integer, primary_key=True, autoincrement=True)
-    ten_the_loai = Column(String(50), unique=True)
-    ma_sach = relationship("MaSach", backref="theloai")
+    ten_the_loai = Column(String(50))
+    ma_sach = relationship("Sach", backref="the_loai", lazy=True)
 
     def __str__(self):
         return self.name
@@ -34,7 +35,7 @@ class TheLoai(db.Model):
 class TacGia(db.Model):
     ma_tac_gia = Column(Integer, primary_key=True, autoincrement=True)
     ten_tac_gia = Column(String(50))
-    ma_sach = relationship("MaSach", backref="tacgia")
+    ma_sach = relationship("Sach", backref="tac_gia", lazy=True)
 
     def __str__(self):
         return self.name
@@ -56,12 +57,12 @@ class Sach(db.Model):
 
 class KhachHang(db.Model):
     ma_khach_hang = Column(Integer, primary_key=True, autoincrement=True)
-    user_name = Column(String(50), unique=True)
-    password = Column(String(20), nullable=False)
+    username = Column(String(50), unique=True)
+    password = Column(String(50), nullable=False)
     ten = Column(String(50), nullable=False)
     dia_chi = Column(String(50))
     so_dien_thoai = Column(String(10))
-    don_hang = relationship("DonHang", backref="khachhang")
+    don_hang = relationship("DonHang", backref="khac_hang")
 
     def __str__(self):
         return self.name
@@ -69,12 +70,12 @@ class KhachHang(db.Model):
 
 class NhanVien(db.Model):
     ma_nhan_vien = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50))
+    password = Column(String(50), nullable=False)
     ten_nhan_vien = Column(String(50), nullable=False)
-    dia_chi = Column(String(50))
     vai_tro = Column(Enum(VaiTro))
-    phieu_nhap_sach = relationship("PhieuNhapSach", backref="nhanvien", lazy=True)
-    hoa_don = relationship("HoaDon", backref="nhanvien")
-    chi_tiet_hoa_don = relationship("ChiTietHoaDon", backref="nhanvien")
+    phieu_nhap_sach = relationship("PhieuNhapSach", backref="nhan_vien", lazy=True)
+    hoa_don = relationship("HoaDon", backref="nhan_vien")
 
     def __str__(self):
         return self.name
@@ -86,7 +87,7 @@ class PhieuNhapSach(db.Model):
     ma_nhan_vien_nhap = Column(
         Integer, ForeignKey(NhanVien.ma_nhan_vien), nullable=False
     )
-    chi_tiet_phieu_nhap = relationship("ChiTietPhieuNhap", backref="phieunhapsach")
+    chi_tiet_phieu_nhap = relationship("ChiTietPhieuNhap", backref="phieu_nhap_sach")
 
     def __str__(self):
         return self.name
@@ -108,7 +109,7 @@ class DonHang(db.Model):
     ma_khach_hang = Column(Integer, ForeignKey(KhachHang.ma_khach_hang), nullable=False)
     phuong_thuc_thanh_toan = Column(Enum(PhuongThucThanhToan), nullable=False)
     ngay_tao = Column(DateTime)
-    chi_tiet_don_hang = relationship("ChiTietDonHang", backref="donhang")
+    chi_tiet_don_hang = relationship("ChiTietDonHang", backref="don_hang")
 
     def __str__(self):
         return self.name
@@ -128,7 +129,7 @@ class HoaDon(db.Model):
     ma_hoa_don = Column(Integer, primary_key=True, autoincrement=True)
     ma_nhan_vien = Column(Integer, ForeignKey(NhanVien.ma_nhan_vien), nullable=False)
     ngay_lap = Column(DateTime)
-    chi_tiet_hoa_don = relationship("ChiTietHoaDon", backref="hoadon")
+    chi_tiet_hoa_don = relationship("ChiTietHoaDon", backref="hoa_don")
 
     def __str__(self):
         return self.name
@@ -147,3 +148,39 @@ class ChiTietHoaDon(db.Model):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
+
+        import json
+
+        #  --- Add tác giả ----
+        # with open('data/tac_gia.json', encoding='utf-8') as f:
+        #     tac_gia = json.load(f)
+        #     for t in tac_gia :
+        #         tac = TacGia(**t)
+        #         db.session.add(tac)
+        # db.session.commit()
+
+        # --- Add thể loại ---
+        with open('data/the_loai.json', encoding='utf-8') as f:
+            the_loai = json.load(f)
+            for t in the_loai :
+                the = TheLoai(**t)
+                db.session.add(the)
+        db.session.commit()
+
+        #  ----  Add Sach ---
+        # with open("data/sach.json", encoding="utf-8") as f:
+        #     sach = json.load(f)
+        #     for s in sach:
+        #         sach = Sach(**s)
+        #         db.session.add(sach)
+        # db.session.commit()
+
+    # Add admin
+    #     import hashlib
+    #
+    #     u = NhanVien(username="admin",
+    #                  password=str(hashlib.md5("123".encode('utf-8')).hexdigest()),
+    #                  ten_nhan_vien="haunguyen",
+    #                  vai_tro=VaiTro.QUAN_LY)
+    #     db.session.add(u)
+    #     db.session.commit()
