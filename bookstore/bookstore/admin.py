7b1e2d701798721dba596d768d170aceba4a84b7
QuocBao <baovo32911@gmail.com>
2024-12-04 23:17:45 +0700

    Add admin.py

---
+++ bookstore/bookstore/admin.py
@@ -0,0 +1,14 @@
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from models import TheLoai
+from bookstore import app, db
+
+
+admin = Admin(app, name="E-commerce Website", template_mode="bootstrap4")
+
+
+class TheLoaiView(ModelView):
+    column_list = ['ma_the_loai', 'ten_the_loai']
+    column_searchable_list = ['ma_the_loai', 'ten_the_loai']
+
+admin.add_view(TheLoaiView(TheLoai, db.session))
