f02ecf16b3f351467553b0c56f112b8ead32f7a7
QuocBao <baovo32911@gmail.com>
2024-12-07 00:55:17 +0700

    Add dao

--- bookstore/bookstore/index.py
+++ bookstore/bookstore/index.py
@@ -1,9 +1,17 @@
-from flask import Flask
-from bookstore import app, db
+import math
+from flask import render_template, request, redirect
+import dao
+from bookstore import app,admin, login
+from flask_login import login_user, logout_user, current_user
+import cloudinary.uploader
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template('index.html')
+
+@login.user_loader
+def load_user(user_id):
+    return dao.get_user_by_id(user_id)
 
 if __name__ == "__main__":
     with app.app_context():
